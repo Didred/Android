b0422dc07eeaa0aaa6dd52c18fd81b9a8a14c876
Andrey_Yurevich <and9019468@yandex.ru>
2018-12-19 02:55:29 +0300

    Added edit profile

--- app/src/main/java/com/example/didred/android/ProfileFragment.java
+++ app/src/main/java/com/example/didred/android/ProfileFragment.java
@@ -1,6 +1,5 @@
 package com.example.didred.android;
 
-import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
@@ -11,163 +10,130 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnSuccessListener;
+import com.example.didred.android.UserProfile;
 import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ProfileFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ProfileFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private TextView phoneNumberView;
+    private TextView fullNameView;
+    private ImageView profileImageView;
+    private ProgressBar progressBar;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
-    public ProfileFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
-        ProfileFragment fragment = new ProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private Button editButton;
+    private Button logoutButton;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 
-    private Button logoutButton;
-    private Button editButton;
-
     @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState){
-        if (FirebaseAuth.getInstance().getCurrentUser() == null) {
-            Navigation.findNavController(view).navigate(R.id.loginFragment);
-            return;
-        }
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+        progressBar = view.findViewById(R.id.progressBar);
+        editButton = view.findViewById(R.id.profileEditButton);
+        logoutButton = view.findViewById(R.id.profileLogoutButton);
+        disableButtons();
 
-        logoutButton = view.findViewById(R.id.logout);
-        logoutButton.setOnClickListener(logout);
+        editButton.setOnClickListener(
+                Navigation.createNavigateOnClickListener(R.id.action_profileFragment_to_profileEditFragment)
+        );
+        logoutButton.setOnClickListener(logoutButtonListener);
 
-        editButton = view.findViewById(R.id.edit);
-        editButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_profileFragment_to_profileEditFragment));
-
-        TextView emailView = view.findViewById(R.id.textView2);
+        TextView emailView = view.findViewById(R.id.profileEmailView);
         String userEmail = FirebaseAuth.getInstance().getCurrentUser().getEmail();
         emailView.setText(userEmail);
 
-        final ImageView profileImageView = view.findViewById(R.id.imageView);
+        phoneNumberView = view.findViewById(R.id.profilePhoneNumberView);
+        fullNameView = view.findViewById(R.id.profileFullNameView);
+
+        profileImageView = view.findViewById(R.id.profileImageView);
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         StorageReference reference = FirebaseStorage.getInstance().getReference().child(user.getUid());
+        reference.getBytes(Long.MAX_VALUE)
+                .addOnSuccessListener(successImageLoadListener)
+                .addOnFailureListener(failureImageLoadListener);
 
-        reference.getBytes(Long.MAX_VALUE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
-            @Override
-            public void onSuccess(byte[] bytes) {
-                Bitmap bmp = BitmapFactory.decodeByteArray(bytes,0,bytes.length);
-                profileImageView.setImageBitmap(bmp);
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception exception) {
-                // Handle any errors
-            }
-        });
-
+        DatabaseReference dbReference = FirebaseDatabase.getInstance().getReference()
+                .child("userProfiles").child(user.getUid());
+        dbReference.addValueEventListener(profileEventListener);
     }
 
-    private View.OnClickListener logout = new View.OnClickListener() {
+    private View.OnClickListener logoutButtonListener = new View.OnClickListener() {
         @Override
-        public void onClick(View v) {
+        public void onClick(final View v) {
             ((MainActivity) getActivity()).cleanArticlesCache();
             FirebaseAuth.getInstance().signOut();
             ((MainActivity) getActivity()).startAuthActivity();
         }
     };
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+    private OnSuccessListener<byte[]> successImageLoadListener = new OnSuccessListener<byte[]>() {
+        @Override
+        public void onSuccess(byte[] bytes) {
+            Bitmap bmp = BitmapFactory.decodeByteArray(bytes,0, bytes.length);
+            profileImageView.setImageBitmap(bmp);
+            enableButtons();
         }
-    }
+    };
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+    private OnFailureListener failureImageLoadListener = new OnFailureListener() {
+        @Override
+        public void onFailure(@NonNull Exception exception) {
+            Log.d("ProfileImage", exception.getMessage());
+            enableButtons();
         }
+    };
+
+    private ValueEventListener profileEventListener = new ValueEventListener() {
+        @Override
+        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            UserProfile userProfile = dataSnapshot.getValue(UserProfile.class);
+            if (userProfile!= null){
+                fullNameView.setText(userProfile.getFullName());
+                phoneNumberView.setText(userProfile.getPhoneNumber());
+            }
+        }
+        @Override
+        public void onCancelled(@NonNull DatabaseError databaseError) {
+            Log.d("ProfileImage", databaseError.getMessage());
+            Toast.makeText(getContext(), R.string.profile_show_error_message, Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    private void disableButtons() {
+        editButton.setEnabled(false);
+        logoutButton.setEnabled(false);
+        progressBar.setVisibility(ProgressBar.VISIBLE);
     }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
+    private void enableButtons() {
+        editButton.setEnabled(true);
+        logoutButton.setEnabled(true);
+        progressBar.setVisibility(ProgressBar.INVISIBLE);
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
 }
